2fd67622b3c457375f93a588098939f91a550de3
Nitin Reddy <reddynitin3@gmail.com>
2022-03-28 12:33:28 +0530

    Added the Select dropdown component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
+import { Modal, Button, Select, Layout, Row, Col } from 'antd';
 import './App.css';
 
-function App() {
+const { Header, Footer, Sider, Content } = Layout;
+
+const { Option } = Select;
+
+const App = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  const children = [];
+  for (let i = 10; i < 36; i++) {
+    children.push(<Option key={i.toString(36) + i}>{i.toString(36) + i}</Option>);
+  }
+
+  function handleChange(value) {
+    console.log(`selected ${value}`);
+  }
+
+  const tagRender = tag => {
+    return (
+      <span>
+        {tag}
+        <Button type="link" onClick={() => { }}>
+          Delete
+        </Button>
+      </span>
+    );
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Layout>
+        <Header>Select dropdown</Header>
+        <Content>
+          <Row>
+            <Col span={12} offset={6}>
+              <Button type="primary" onClick={showModal}>
+                Open Modal
+              </Button>
+              <Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+                <Select
+                  mode="multiple"
+                  allowClear
+                  style={{ width: '100%' }}
+                  placeholder="Please select"
+                  defaultValue={['a10', 'c12']}
+                  onChange={handleChange}
+                  tagRender={tagRender}
+                >
+                  {children}
+                </Select>
+              </Modal>
+            </Col>
+          </Row>
+        </Content>
+      </Layout>
+    </>
   );
-}
+};
 
 export default App;
